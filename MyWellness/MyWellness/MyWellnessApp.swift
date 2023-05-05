d9954aec9f5b5c3e68740b4a20f0262455d1ae3e
ZL-Asica <lzr2002qd@gmail.com>
2023-05-04 22:11:31 -0700

    Project create

---
+++ MyWellness/MyWellness/MyWellnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWellnessApp.swift
+//  MyWellness
+//
+//  Created by ZL Asica on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct MyWellnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
