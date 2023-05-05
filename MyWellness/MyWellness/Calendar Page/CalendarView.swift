aabe9adfca5e2da0d7fc0f54e3a06bddbf056845
ZL-Asica <lzr2002qd@gmail.com>
2023-05-05 15:41:23 -0700

    add no reason miss file - CalendarView.swift

---
+++ MyWellness/MyWellness/Calendar Page/CalendarView.swift
@@ -0,0 +1,21 @@
+//
+//  CalendarView.swift
+//  MyWellness
+//
+//  Created by ZL Asica on 5/4/23.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text("Calendar")
+    }
+}
+
+struct CalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarView()
+    }
+}
+
